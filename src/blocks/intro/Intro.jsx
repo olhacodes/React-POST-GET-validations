9cca0281d70a142f1ff0acc7ba9a9d301a7dc3dc
olha.codes <57961694+olhacodes@users.noreply.github.com>
2022-10-15 14:19:37 +0300

    added scroll to in the intro section

--- src/blocks/intro/Intro.jsx
+++ src/blocks/intro/Intro.jsx
@@ -1,4 +1,5 @@
-import React from 'react'; 
+import React, { useContext } from 'react'; 
+import { UsersContext } from '../../context/UsersContext';
 import Button from '../../components/Button/Button';
 import './intro.styles.less';
 
@@ -7,13 +8,16 @@ const introStyle = {
 }
 
 function Intro() {
+  const { formRef } = useContext(UsersContext);
+  const scrollToForm = () => formRef.current?.scrollIntoView({behavior: 'smooth'});
+  
   return (
     <div className='intro' style={introStyle}>
         <div className="intro__container">
           <div className="intro__inner">
             <h1>Test assignment for front-end developer</h1>
             <p>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</p>
-          <Button>Sign up</Button>
+          <Button onClick={scrollToForm}>Sign up</Button>
           </div>
         </div> 
     </div>
